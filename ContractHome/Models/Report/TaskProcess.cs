7805916870a7dfb24b3187aa5f06c33eb640189a
ripsea0505@hotmail.com <ripsea0505@hotmail.com>
2024-10-18 16:53:44 +0800

    feat[簽署歷程文件]: ViewRenderService套用產製html, 及html套用ironpdf產製pdf

---
+++ ContractHome/Models/Report/TaskProcess.cs
@@ -0,0 +1,31 @@
+﻿using CommonLib.Core.Utility;
+using ContractHome.Models.DataEntity;
+using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel;
+
+namespace ContractHome.Models.Rpt
+{
+    public class TaskProcess
+    {
+        public string FileName { get; set; }
+        public string FileNo { get; set; }
+        public string Email { get; set; }
+        public string PublishedDateTime { get; set; }
+        public string FinishedDateTime { get; set; }
+        public IEnumerable<Operator> Operators { get; set; }
+        public IEnumerable<Process> Processes { get; set; }
+        public string TemplateItem => @$"~/Views/Report/{this.GetType().Name}.cshtml";
+
+
+        public class Operator
+        {
+            public string Name { get; set; }
+        }
+
+        public class Process
+        {
+            public string Name { get; set; }
+            public string DateTime { get; set; }
+        }
+    }
+}
