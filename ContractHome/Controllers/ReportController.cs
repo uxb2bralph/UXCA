8d8619d1f88e0a3cbaa2838400e720c1e59dd015
ripsea0505@hotmail.com <ripsea0505@hotmail.com>
2025-01-22 11:13:44 +0800

    feat: 簽署歷程報表產製並轉pdf

--- ContractHome/Controllers/ReportController.cs
+++ ContractHome/Controllers/ReportController.cs
@@ -1,12 +1,15 @@
 ﻿using ContractHome.Helper;
 using ContractHome.Models.DataEntity;
-using ContractHome.Models.Rpt;
+using ContractHome.Models.Report;
 using ContractHome.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq.Dynamic.Core;
 using CommonLib.Core.Utility;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Html;
+using ContractHome.Models.Dto;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Drawing;
 
 namespace ContractHome.Controllers
 {
@@ -15,17 +18,20 @@ namespace ContractHome.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly TaskProcess _taskProcess;
         private readonly IViewRenderService _viewRenderService;
-        public ReportController(ILogger<HomeController> logger, IServiceProvider serviceProvider, TaskProcess taskProcess
-            , IViewRenderService viewRenderService
+        private readonly string dateTimeFormat = "yyyy/MM/dd HH:mm:ss";
+        private readonly BaseResponse _baseResponse;
+        public ReportController(ILogger<HomeController> logger, IServiceProvider serviceProvider
+            , IViewRenderService viewRenderService, BaseResponse baseResponse
             ) : base(serviceProvider)
         {
             _logger = logger;
-            _taskProcess = taskProcess;
+            _taskProcess = new TaskProcess();
             _viewRenderService = viewRenderService;
+            _baseResponse = baseResponse;
         }
 
         //[UserAuthorize]
-        public async Task<ActionResult> TaskProcessAsync([FromBody]SignContractViewModel viewModel)
+        public async Task<IActionResult> TaskProcessAsync([FromBody]SignContractViewModel viewModel)
         {
             ViewBag.ViewModel = viewModel;
             if (viewModel.KeyID != null)
@@ -37,20 +43,34 @@ namespace ContractHome.Controllers
                 .Where(c => c.ContractID == viewModel.ContractID)
                 .FirstOrDefault();
 
+            if (contract==null) { return BadRequest(_baseResponse.ErrorMessage($"合約{viewModel.KeyID}不存在")); }
+
+            var logList = contract.CDS_Document.DocumentProcessLog;
+
             _taskProcess.FileNo = contract.ContractNo;
             _taskProcess.FileName = contract.Title;
-            //_taskProcess.Email = Contract新增起約人
-            _taskProcess.Processes = contract.CDS_Document.DocumentProcessLog.Select(x => new TaskProcess.Process() { Name = x.ActorID.ToString()! });
-            _taskProcess.Operators = contract.ContractingParty.Select(x => new TaskProcess.Operator() { Name = x.ContractID.ToString() });
+            _taskProcess.TaskProcessDateTime = DateTime.Now.ToString(dateTimeFormat);
+            _taskProcess.CreateUser = contract.UserProfile;
+            _taskProcess.PublishedDateTime = logList.Min(x => x.LogDate).ToString(dateTimeFormat);
+            _taskProcess.FinishedDateTime = logList.Max(x => x.LogDate).ToString(dateTimeFormat);
+            _taskProcess.Processes = logList.Select(x => 
+                new TaskProcess.Process(
+                    taskDateTime:x.LogDate.ToString(dateTimeFormat), 
+                    taskDesc: CDS_Document.StepNaming[x.StepID], 
+                    email: x.UserProfile.EMail,
+                    clientIP: x.ClientIP,
+                    clientDevice: x.ClientDevice));
+            _taskProcess.Operators = contract.ContractingUser.Select(x => 
+                    new TaskProcess.Operator(email: x.UserProfile.EMail, region: x.UserProfile.Region));
 
             var rptViewRenderString = await _viewRenderService.RenderToStringAsync(
                 viewName: _taskProcess.TemplateItem,
                 model: _taskProcess);
 
             var renderer = new ChromePdfRenderer();
-            //PdfDocument pdf = renderer.RenderHtmlAsPdf(rptViewRenderString).SaveAs("test.pdf");
             PdfDocument pdf = await renderer.RenderHtmlAsPdfAsync(rptViewRenderString);
             return File(pdf.BinaryData, "application/pdf", "viewToPdfMVCCore.pdf");
+            //PdfDocument pdf = renderer.RenderHtmlAsPdf(rptViewRenderString).SaveAs("test.pdf");
         }
 
     }
--- ContractHome/Models/Report/TaskProcess.cs
+++ ContractHome/Models/Report/TaskProcess.cs
@@ -3,29 +3,51 @@ using ContractHome.Models.DataEntity;
 using Microsoft.IdentityModel.Tokens;
 using System.ComponentModel;
 
-namespace ContractHome.Models.Rpt
+namespace ContractHome.Models.Report
 {
     public class TaskProcess
     {
-        public string FileName { get; set; }
-        public string FileNo { get; set; }
-        public string Email { get; set; }
-        public string PublishedDateTime { get; set; }
-        public string FinishedDateTime { get; set; }
-        public IEnumerable<Operator> Operators { get; set; }
-        public IEnumerable<Process> Processes { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public string FileNo { get; set; } = string.Empty;
+        public UserProfile CreateUser { get; set; }
+        public string TaskProcessDateTime { get; set; } = string.Empty;
+        public string PublishedDateTime { get; set; } = string.Empty;
+        public string FinishedDateTime { get; set; } = string.Empty;
+        public IEnumerable<Operator>? Operators { get; set; }
+        public IEnumerable<Process>? Processes { get; set; }
         public string TemplateItem => @$"~/Views/Report/{this.GetType().Name}.cshtml";
 
 
         public class Operator
         {
-            public string Name { get; set; }
+            public string Email { get; set; }
+            private string Region { get; set; }
+            public string RegionDesc =>
+                this.Region.Equals("O") ? "工商憑證" : (this.Region.Equals("E") ? "企業憑證" : "其他");
+
+            public Operator(string email, string region)
+            {
+                Email = email;
+                Region = region;
+            }
         }
 
         public class Process
         {
-            public string Name { get; set; }
-            public string DateTime { get; set; }
+            public string TaskDateTime { get; set; }
+            public string TaskDesc { get; set; }
+            public string Email { get; set; }
+            public string ClientIP { get; set; }
+            public string ClientDevice { get; set; }
+
+            public Process(string taskDateTime, string taskDesc, string email, string clientIP, string clientDevice)
+            {
+                TaskDateTime = taskDateTime;
+                TaskDesc = taskDesc;
+                Email = email;
+                ClientIP = clientIP;
+                ClientDevice = clientDevice;
+            }
         }
     }
 }
